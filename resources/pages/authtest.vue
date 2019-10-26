2b10a6102b8086133af4bbb19c306a7b229dfe3f
Elihu Cruz <elihuacruz@gmail.com>
2019-10-26 00:07:01 -0500

    auth configuration

---
+++ resources/pages/authtest.vue
@@ -0,0 +1,18 @@
+<template>
+  <section class="container">
+    <button @click="login">
+      login
+    </button>
+  </section>
+</template>
+
+<script>
+export default {
+  methods: {
+    async login () {
+      const data = { email: 'example@yopmail.com', password: 'here'}
+      await this.$auth.loginWith('local', { data })
+    }
+  }
+}
+</script>
