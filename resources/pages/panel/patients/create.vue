15f13ecd8d44d7695cfaabd278284dc2e1465104
Víctor Gil Calderón <victor19961996@live.com.mx>
2019-10-26 17:09:34 -0500

    Avanse crear pasiente

--- resources/pages/panel/patients/create.vue
+++ resources/pages/panel/patients/create.vue
@@ -3,26 +3,34 @@
     Title(title="Registrar paciente")
     p Create
 
-    p {{ doctorSelected }}
+    .column.is-12-desktop.is-12-mobile
+      .columns.is-multiline.is-mobile.is-centered
+        .column.is-9-desktop.is-12-mobile
+          form( @submit.prevent="validateBeforeSubmit" ref='form')
+            b-field( label="Nombre Completo" :type="{'is-danger': errors.has('name')}" :message="errors.first('name')" )
+              b-input( v-model="name" name="name" v-validate="'required|max:500'" maxlength="254")
 
-    .column.is-12-desktop.is-11-mobile
-      // - Seleccionar Doctor
-      b-field(
-        label="Selecciona un doctor *"
-        :type="{'is-danger': errors.has('doctor')}"
-        :message="errors.first('doctor')"
-        maxlength="500"
-      )
-        b-autocomplete(
-          v-model="doctor"
-          :data="doctors"
-          name="doctor"
-          v-validate="'required'"
-          placeholder="Selecciona un doctor"
-          :open-on-focus="true"
-          field="username"
-          @select="option => doctorSelected = option"
-        )
+            // - Seleccionar Doctor
+            b-field( label="Selecciona un doctor" :type="{'is-danger': errors.has('doctor')}" :message="errors.first('doctor')" )
+              b-autocomplete(
+                v-model="doctor"
+                :data="doctors"
+                name="doctor"
+                v-validate="'required'"
+                placeholder="Selecciona un doctor"
+                :open-on-focus="true"
+                field="username"
+                @select="option => doctorSelected = option"
+              )
+
+            b-field( label="Invitado (familiar)" :type="{'is-danger': errors.has('guest_id')}" :message="errors.first('guest_id')" )
+              b-input( v-model="guest_id" name="guest_id" v-validate="'required|max:500'" )
+
+            b-field( label="Cama" :type="{'is-danger': errors.has('bed')}" :message="errors.first('bed')" )
+              b-input( v-model="bed" name="bed" v-validate="'required|max:500'" )
+
+            button.button.has-background-pink.has-text-white.is-pulled-right.mb-4.pl-2.pr-2( type="submit" ) Crear paciente
+
 </template>
 
 <script>
@@ -39,6 +47,9 @@ export default {
 		await store.dispatch('users/list')
 	},
   data: () => ({
+    name: null,
+    guest_id: null,
+    bed: null,
     // Select filter autocomplete,
     doctor: '', // buscador
     doctorSelected: null, // seleccionado
@@ -55,6 +66,36 @@ export default {
       this.$store.commit('users/setPage', this.page)
       this.$store.commit('users/setSearch', this.doctor)
       this.$store.dispatch('users/list')
+    },
+		validateBeforeSubmit() {
+			this.$validator.validateAll().then((result) => {
+				if (result) {
+					this.AddNewPatient()
+					this.$buefy.toast.open({
+						message: 'Formulario enviado.',
+						type: 'is-success',
+						position: 'is-bottom'
+					})
+
+					return;
+				}
+				this.$buefy.toast.open({
+					message: 'El formulario no es valido! Por favor verifica los campos.',
+					type: 'is-danger',
+					position: 'is-bottom'
+				})
+      })
+    },
+    async AddNewPatient(){
+      const data = {
+        name: this.name,
+        doctor_id: this.doctorSelected.id,
+        guest_id: this.guest_id,
+				bed: this.bed
+			}
+			// Crea nuevo usuario en store
+			await this.$store.dispatch('patients/create', data)
+			this.$router.push("/panel/patients")
     }
   },
   computed: {
