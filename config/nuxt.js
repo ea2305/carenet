cb11c5d2388683e088aec737b4a12aeae8dc53ac
Víctor Gil Calderón <victor19961996@live.com.mx>
2019-10-26 01:07:36 -0500

    Buetify

--- config/nuxt.js
+++ config/nuxt.js
@@ -27,8 +27,14 @@ module.exports = {
       { rel: 'stylesheet', href: 'https://use.fontawesome.com/releases/v5.2.0/css/all.css', integrity: 'sha384-hWVjflwFxL6sNzntih27bfxkr27PmbbK/iSvJ+a4+0owXq79v+lsFkW54bOGbiDQ', crossorigin: 'anonymous'}
     ]
   },
+     /**
+   * Project modules
+   */
   modules: [
     '@nuxtjs/auth',
+    'nuxt-buefy',
+    // '@nuxtjs/dotenv',
+    // '@nuxtjs/axios',
   ],
   plugins: [],
   auth: {
