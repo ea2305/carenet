d9152ea3802f7fe7e534745160fb0afe35292764
Elihu Cruz <elihuacruz@gmail.com>
2019-10-26 12:22:51 -0500

    Udpate structura for patients

---
+++ resources/pages/panel/patients/create.vue
@@ -0,0 +1,16 @@
+<template lang="pug">
+  main
+    Title(title="Registrar paciente")
+    p Create
+</template>
+
+<script>
+import Title from '~/components/Layout/Title'
+export default {
+  layout: 'auth',
+  middleware: 'auth',
+  components: {
+    Title
+  },
+}
+</script>
---
+++ resources/pages/panel/patients/index.vue
@@ -0,0 +1,16 @@
+<template lang="pug">
+  main
+    Title(title="Listado de pacientes")
+    p index
+</template>
+
+<script>
+import Title from '~/components/Layout/Title'
+export default {
+  layout: 'auth',
+  middleware: 'auth',
+  components: {
+    Title
+  },
+}
+</script>
