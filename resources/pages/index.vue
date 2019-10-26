3eec7c5405b4da99d13e89d05dec44f1d2d205f1
Víctor Gil Calderón <victor19961996@live.com.mx>
2019-10-26 01:46:29 -0500

    Index lo deje como inicio

--- resources/pages/index.vue
+++ resources/pages/index.vue
@@ -1,46 +1,5 @@
 <template>
   <section class="container">
     <div class="hola">Holamundo</div>
-    <div class="column is-12">
-      <div class="columns is-multiline is-mobile">
-        <div class="column"> Hola</div>
-        <div class="column"> Hola</div>
-        <div class="column"> Hola</div>
-        <div class="column"> Hola</div>
-        <div class="column is-6"> Hola</div>
-      </div>
-
-              <b-field label="Name">
-            <b-input value="Kevin Garvey"></b-input>
-        </b-field>
-
-        <b-field label="Email"
-            type="is-danger"
-            message="This email is invalid">
-            <b-input type="email"
-                value="john@"
-                maxlength="30">
-            </b-input>
-        </b-field>
-
-        <b-field label="Username"
-            type="is-success"
-            message="This username is available">
-            <b-input value="johnsilver" maxlength="30"></b-input>
-        </b-field>
-
-        <b-field label="Password"
-            type="is-warning"
-            :message="['Password is too short', 'Password must have at least 8 characters']">
-            <b-input value="123" type="password" maxlength="30"></b-input>
-        </b-field>
-
-        <b-field label="Subject">
-            <b-select placeholder="Select a subject">
-                <option value="1">Option 1</option>
-                <option value="2">Option 2</option>
-            </b-select>
-        </b-field>
-    </div>
   </section>
 </template>
