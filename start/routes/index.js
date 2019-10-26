d510f615b511ce5b03988b485ccc9880b7c5f58b
CrisTovilla <tovilla21@gmail.com>
2019-10-26 01:26:28 -0500

    User Api

--- start/routes/index.js
+++ start/routes/index.js
@@ -4,15 +4,12 @@
 |--------------------------------------------------------------------------
 | Routes
 |--------------------------------------------------------------------------
-|
-| Http routes are entry points to your web application. You can create
-| routes for different URL's and bind Controller actions to them.
-|
-| A complete guide on routing is available here.
-| http://adonisjs.com/guides/routing
-|
 */
 
 const Route = use('Route')
+
+// Router split files
+require('./auth')
+
 
 Route.any('*', 'NuxtController.render')
